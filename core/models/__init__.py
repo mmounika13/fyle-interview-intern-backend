c87091842075c405fd3ee675051912dc09c5133c
mmounika13 <167684224+mmounika13@users.noreply.github.com>
2024-09-18 16:47:08 +0530

    Update __init__.py

---
+++ core/models/__init__.py
@@ -0,0 +1,5 @@
+from .assignments import Assignment  # Import Assignment
+from .teachers import Teacher  # Import Teacher (if applicable)
+
+# If you have db defined here, you can include it
+from . import db  # Import db if it is defined in another file
